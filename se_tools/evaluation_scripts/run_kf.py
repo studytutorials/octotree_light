837672dd7e85cf4bb578a9f919c032923d2266c8
Sotiris Papatheodorou <sotirisp@protonmail.com>
2019-03-25 17:41:13 +0000

    Improve resulting .log file
    Improved column alignment and added columns for implementation (SDF or
    OFUSION) and volume resolution.

--- se_tools/evaluation_scripts/run_kf.py
+++ se_tools/evaluation_scripts/run_kf.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     # for sequence in ICL + TUM_RGB_FR1 + TUM_RGB_FR2 + TUM_RGB_FR3:
     for sequence in [ICL_NUIM_LIV_2, TUM_RGB_FR3_DESK]:
         for resol in [512]:
-            for version in ['tsdf']:
+            for version in ['tsdf', 'ofusion', 'multirestsdf']:
                 kernel_data = []
                 mu = 0.1
                 algorithm.impl = version
@@ -46,23 +46,30 @@ if __name__ == "__main__":
                 res = algorithm.run(sequence)
                 res['sequence'] = sequence.descr
                 res['noise_factor'] = mu
+                res['implementation'] = version
+                res['resolution'] = resol
                 run_results[run_counter] = res
                 run_counter += 1
                 kernel_data.append(res)
 
     with open(results_dir + '/resume.log', 'w') as f:
-        f.write('{:>10}\t{:>10}\t{:>10}\t{:>10}\t{:>10}\t{:>10}\t{:>10}\n'\
+        f.write('{:<15} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}\n'\
                 .format('dataset', 'noise_factor', 'ATE',
                 'preprocessing',
                 'integration',
                 'raycasting',
-                'computation'))
+                'computation',
+                'implementation',
+                'resolution'))
         for k, d in run_results.iteritems():
             data = d['data']
-            f.write('{:>10}\t{:>10.4f}\t{:>10.4f}\t{:>10.4f}\t{:>10.4f}\t{:>10.4f}\t{:>10.4f}\n'.format(d['sequence'],
+            f.write('{:<15} {:<15.4f} {:<15.4f} {:<15.4f} {:<15.4f} {:<15.4f} {:<15.4f} {:<15} {:<15}\n'.format(d['sequence'],
                         float(d['noise_factor']),
                         float(d['ate_mean']),
                         float(data['preprocessing']['mean']),
                         float(data['integration']['mean']),
                         float(data['raycasting']['mean']),
-                        float(data['computation']['mean'])))
+                        float(data['computation']['mean']),
+                        d['implementation'],
+                        int(d['resolution'])))
+
