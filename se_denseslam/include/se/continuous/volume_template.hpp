emanuelev/supereight

Implement strided interp/grad in continuous volume

se_denseslam/include/se/continuous/volume_template.hpp
@@ -93,12 +93,47 @@ class VolumeTemplate {
       return _map_index->interp(discrete_pos, select);
     }
 
+  /*! \brief Interp voxel value at metric position  (x,y,z)
+   * \param pos three-dimensional coordinates in which each component belongs 
+   * to the interval [0, _dim]
+   * \param stride distance between neighbouring sampling point, in voxels.
+   * Must be >= 1
+   * \return signed distance function value at voxel position (x, y, z)
+   */
+    template <typename FieldSelector>
+    float interp(const Eigen::Vector3f& pos, const int h, 
+        FieldSelector select) const {
+      const float inverseVoxelSize = _size / _dim;
+      Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
+      return _map_index->interp(discrete_pos, h, select);
+    }
+
+    /*! \brief Compute gradient at metric position  (x,y,z)
+     * \param pos three-dimensional coordinates in which each component belongs 
+     * to the interval [0, _dim]
+     * \return signed distance function value at voxel position (x, y, z)
+     */
     template <typename FieldSelector>
     Eigen::Vector3f grad(const Eigen::Vector3f& pos, FieldSelector select) const {
 
       const float inverseVoxelSize = _size / _dim;
       Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
-      return _map_index->grad(discrete_pos, select);
+      return _map_index->grad(discrete_pos, 1.f, select);
+    }
+
+    /*! \brief Compute gradient at metric position  (x,y,z)
+     * \param pos three-dimensional coordinates in which each component belongs 
+     * to the interval [0, _dim]
+     * \param stride distance between neighbouring sampling point, in voxels.
+     * Must be >= 1
+     * \return signed distance function value at voxel position (x, y, z)
+     */
+    template <typename FieldSelector>
+    Eigen::Vector3f grad(const Eigen::Vector3f& pos, const int h, 
+        FieldSelector select) const {
+      const float inverseVoxelSize = _size / _dim;
+      Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
+      return _map_index->grad(discrete_pos, h, select);
     }
 
     unsigned int _size;

se_denseslam/src/kfusion/rendering_impl.hpp
@@ -35,6 +35,7 @@ inline Eigen::Vector4f raycast(const Volume<SDF>& volume, const Eigen::Vector3f&
     const Eigen::Vector3f& direction, const float tnear, const float tfar, 
     const float mu, const float step, const float largestep) { 
 
+  const int voxelsize = volume._dim / volume._size;
   auto select_depth = [](const auto& val){ return val.x; };
   if (tnear < tfar) {
     // first walk with largesteps until we found a hit
@@ -53,7 +54,7 @@ inline Eigen::Vector4f raycast(const Volume<SDF>& volume, const Eigen::Vector3f&
         }
         f_tt = data.x;
         if(f_tt <= 0.1 && f_tt >= -0.5f){
-          f_tt = volume.interp(position, select_depth);
+          f_tt = volume.interp(position, 1, select_depth);
         }
         if (f_tt < 0)                  // got it, jump out of inner loop
           break;

studytutorials/octotree_light

se_denseslam/include/se/continuous/volume_template.hpp
@@ -93,12 +93,47 @@ class VolumeTemplate {
       return _map_index->interp(discrete_pos, select);
     }
 
+  /*! \brief Interp voxel value at metric position  (x,y,z)
+   * \param pos three-dimensional coordinates in which each component belongs
+   * to the interval [0, _extent]
+   * \param stride distance between neighbouring sampling point, in voxels.
+   * Must be >= 1
+   * \return signed distance function value at voxel position (x, y, z)
+   */
+    template <typename FieldSelector>
+    float interp(const Eigen::Vector3f& pos, const int h,
+        FieldSelector select) const {
+      const float inverseVoxelSize = _size / _extent;
+      Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
+      return _map_index->interp(discrete_pos, h, select);
+    }
+
+    /*! \brief Compute gradient at metric position  (x,y,z)
+     * \param pos three-dimensional coordinates in which each component belongs
+     * to the interval [0, _extent]
+     * \return signed distance function value at voxel position (x, y, z)
+     */
     template <typename FieldSelector>
     Eigen::Vector3f grad(const Eigen::Vector3f& pos, FieldSelector select) const {
 
       const float inverseVoxelSize = _size / _extent;
       Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
-      return _map_index->grad(discrete_pos, select);
+      return _map_index->grad(discrete_pos, 1.f, select);
+    }
+
+    /*! \brief Compute gradient at metric position  (x,y,z)
+     * \param pos three-dimensional coordinates in which each component belongs
+     * to the interval [0, _extent]
+     * \param stride distance between neighbouring sampling point, in voxels.
+     * Must be >= 1
+     * \return signed distance function value at voxel position (x, y, z)
+     */
+    template <typename FieldSelector>
+    Eigen::Vector3f grad(const Eigen::Vector3f& pos, const int h,
+        FieldSelector select) const {
+      const float inverseVoxelSize = _size / _extent;
+      Eigen::Vector3f discrete_pos = inverseVoxelSize * pos;
+      return _map_index->grad(discrete_pos, h, select);
     }
 
     unsigned int _size;

se_denseslam/src/kfusion/rendering_impl.hpp
@@ -40,6 +40,7 @@ inline Eigen::Vector4f raycast(const Volume<SDF>&     volume,
                                const float            step,
                                const float            largestep) {
 
+  const int voxelsize = volume._extent / volume._size;
   auto select_depth = [](const auto& val){ return val.x; };
   if (tnear < tfar) {
     // first walk with largesteps until we found a hit
@@ -58,7 +59,7 @@ inline Eigen::Vector4f raycast(const Volume<SDF>&     volume,
         }
         f_tt = data.x;
         if (f_tt <= 0.1 && f_tt >= -0.5f) {
-          f_tt = volume.interp(position, select_depth);
+          f_tt = volume.interp(position, 1, select_depth);
         }
         if (f_tt < 0)                  // got it, jump out of inner loop
           break;