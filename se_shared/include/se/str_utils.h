293e6be77a0227f4ae59f0fc1e0e3f64f50b72df
Sotiris Papatheodorou <sotirisp@protonmail.com>
2019-11-11 19:56:20 +0000

    Add extra parameter to splitString
    The default parameter value replicates the old behavior.

--- se_shared/include/se/str_utils.h
+++ se_shared/include/se/str_utils.h
@@ -1,5 +1,5 @@
-#ifndef STR_UTILS_H
-#define STR_UTILS_H
+#ifndef __STR_UTILS_H
+#define __STR_UTILS_H
 
 #include <sstream>
 #include <string>
@@ -8,22 +8,27 @@
 /**
  * Split a string into a vector of substrings based on a delimiter.
  *
- * @note The contents of `elems` are not deleted, substrings are just pushed
- * back.
- *
  * \param[in] s The string to split.
  * \param[in] delim The delimiter to use.
+ * \param[in] ignore_consec Treat consecutive delimiters as a single delimiter.
  * \return The vector containing the substrings.
  */
-static std::vector<std::string> splitString(const std::string& s,
-                                            const char         delim) {
+static std::vector<std::string> splitString(
+    const std::string& s,
+    const char         delim,
+    const bool         ignore_consec = false) {
+
   std::vector<std::string> elems;
   std::stringstream ss(s);
   std::string item;
   while (std::getline(ss, item, delim)) {
-    elems.push_back(item);
+    // Empty items result from consecutive occurences of the delimiter.
+    if (!ignore_consec || (ignore_consec && (item.size() > 0))) {
+      elems.push_back(item);
+    }
   }
   return elems;
 }
 
 #endif
+
