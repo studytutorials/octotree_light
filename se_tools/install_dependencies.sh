3badbfbe1789390789112696ea410e6e9574c35f
Sotiris Papatheodorou <sotirisp@protonmail.com>
2020-03-18 13:38:24 +0200

    Improve dependency installation script
    - Made more robust.
    - Now downloads files in /tmp instead of $HOME.

--- se_tools/install_dependencies.sh
+++ se_tools/install_dependencies.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+#!/bin/sh
+# Install the required and optional dependencies for supereight
+#
+# SPDX-FileCopyrightText: 2019-2020 Sotiris Papatheodorou
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -eu
+IFS="$(printf '%b_' '\t\n')" ; IFS="${IFS%_}"
 
 # Required dependencies
-sudo apt -y install build-essential git cmake libeigen3-dev libgtest-dev
+sudo apt-get --yes install build-essential git cmake libeigen3-dev libgtest-dev
+
+# Required source dependencies
+sophus_dir='/tmp/Sophus'
+rm -rf "$sophus_dir"
+mkdir -p "$sophus_dir"
+git clone --depth=1 https://github.com/strasdat/Sophus.git "$sophus_dir"
+mkdir -p "$sophus_dir/build"
+cd "$sophus_dir/build" && cmake -DCMAKE_BUILD_TYPE=Release ..
+cd "$sophus_dir/build" && make && make test && sudo make install
 
 # Optional dependencies
-sudo apt -y install freeglut3-dev libopenni2-dev libpapi-dev qtbase5-dev
-
-# Source dependencies
-sophus_dir="$HOME/Documents/Source/Sophus"
-mkdir -p "$sophus_dir/build"
-git clone --depth=1 https://github.com/strasdat/Sophus.git "$sophus_dir"
-cd "$sophus_dir/build" && cmake -DCMAKE_BUILD_TYPE=Release ..
-cd "$sophus_dir/build" && make && sudo make install
+sudo apt-get --yes install freeglut3-dev libopenni2-dev libpapi-dev \
+    qtbase5-dev python3 python3-numpy
 
